afbdf60a79e6ab3a19f20f16d5192f4c46c516f5
William Kirk <billykirk@williams-mbp.lan>
2019-08-13 21:24:46 -0500

    Explicity importing only the filter function from lodash

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import _ from "lodash"
+import { filter } from "lodash"
 import axios from "axios"
 
 axios.get("https://jsonplaceholder.typicode.com/posts")
     .then(function (response) {
-        let filteredResults = _.filter(response.data, { userId: 1 })
+        let filteredResults = filter(response.data, { userId: 1 })
         console.log(filteredResults)
     })
     .catch(function (error) {
