51b558ed2d77177796fe844b652cf8484592bb41
William Kirk <billykirk@williams-mbp.lan>
2019-08-13 21:25:05 -0500

    Building after changes to lodash imports

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const lodash_1 = __importDefault(require("lodash"));
+const lodash_1 = require("lodash");
 const axios_1 = __importDefault(require("axios"));
 axios_1.default.get("https://jsonplaceholder.typicode.com/posts")
     .then(function (response) {
-    let filteredResults = lodash_1.default.filter(response.data, { userId: 1 });
+    let filteredResults = lodash_1.filter(response.data, { userId: 1 });
     console.log(filteredResults);
 })
     .catch(function (error) {
