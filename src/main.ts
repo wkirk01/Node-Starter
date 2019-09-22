46d7da1cecabc8468fd37d7f9b9f5916b0ae0b25
William Kirk <billykirk@williams-mbp.lan>
2019-09-21 23:13:18 -0500

    Testing

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-export function sayHello(){
-    console.log("Hello")
+export function get(url: string) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(json => resolve(json))
+            .catch(error => reject(error))
+    });
 }
+
